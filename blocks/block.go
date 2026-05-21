fbff3633fd6397ee483843e9a3313e071bc1c5fd
Mihajlo Djordjevic <midofey.official@gmail.com>
2026-05-21 19:14:08 +0200

    Move RenderBlock into block.go

--- blocks/block.go
+++ blocks/block.go
@@ -1,5 +1,7 @@
 package blocks
 
+import rl "github.com/gen2brain/raylib-go/raylib"
+
 type Block int
 
 const (
@@ -14,3 +16,28 @@ const (
 // Log
 // Leaves
 )
+
+func RenderBlock(block Block, x, y, z int) {
+	switch block {
+	case Air:
+		return
+	case Grass:
+		rl.DrawCube(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.DarkGreen)
+		rl.DrawCubeWires(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.Green)
+	case Stone:
+		rl.DrawCube(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.Gray)
+		rl.DrawCubeWires(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.DarkGray)
+	case Dirt:
+		rl.DrawCube(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.Brown)
+		rl.DrawCubeWires(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.DarkBrown)
+	case Water:
+		rl.DrawCube(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.Blue)
+		rl.DrawCubeWires(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.DarkBlue)
+	case Bedrock:
+		rl.DrawCube(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.Black)
+		rl.DrawCubeWires(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.Gray)
+	case Snow:
+		rl.DrawCube(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.White)
+		rl.DrawCubeWires(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.Gray)
+	}
+}
