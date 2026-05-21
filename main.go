d046832539c847a6dcb70a953e0a4508e43a899b
Mihajlo Djordjevic <midofey.official@gmail.com>
2026-05-21 22:36:37 +0200

    Merge branch 'grafika' into 'navigacija'

--- main.go
+++ main.go
@@ -1,44 +1,20 @@
 package main
 
 import (
+	reljef "main/Reljef"
+
 	rl "github.com/gen2brain/raylib-go/raylib"
 
-	"go-minecraft/blocks"
-	nav "go-minecraft/internal/instructions"
-	"go-minecraft/internal/world"
+	//"main/blocks"
+	"main/world"
 )
 
-func RenderBlock(block blocks.Block, x, y, z int) {
-	switch block {
-	case blocks.Air:
-		return
-	case blocks.Grass:
-		rl.DrawCube(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.DarkGreen)
-		rl.DrawCubeWires(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.Green)
-	case blocks.Stone:
-		rl.DrawCube(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.Gray)
-		rl.DrawCubeWires(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.DarkGray)
-	case blocks.Dirt:
-		rl.DrawCube(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.Brown)
-		rl.DrawCubeWires(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.DarkBrown)
-	case blocks.Water:
-		rl.DrawCube(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.Blue)
-		rl.DrawCubeWires(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.DarkBlue)
-	case blocks.Bedrock:
-		rl.DrawCube(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.Black)
-		rl.DrawCubeWires(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.Gray)
-	case blocks.Snow:
-		rl.DrawCube(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.White)
-		rl.DrawCubeWires(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.Gray)
-	}
-}
-
 func main() {
-	rl.InitWindow(800, 600, "Petnica Minecraft")
+	rl.InitWindow(800, 600, "Raylib Go - 3D Kocka i Skakanje")
 	defer rl.CloseWindow()
 
 	camera := rl.Camera3D{}
-	camera.Position = rl.NewVector3(4.0, 2.0, 4.0)
+	camera.Position = rl.NewVector3(4.0, 10.0, 4.0)
 	camera.Target = rl.NewVector3(0.0, 1.0, 0.0)
 	camera.Up = rl.NewVector3(0.0, 1.0, 0.0)
 	camera.Fovy = 60.0
@@ -47,18 +23,16 @@ func main() {
 	rl.DisableCursor()
 	rl.SetTargetFPS(60)
 
-	w := world.NewDefault()
-
 	var verticalVelocity float32 = 0.0
 	const gravity float32 = -0.6
 	const jumpForce float32 = 0.15
-	const groundLevel float32 = 2.0
+	const groundLevel float32 = 10.0
 	var isGrounded bool = true
+
+	generatedChunk := reljef.GenerateChunk(0, 0, 0.1, 8, 0, 1)
 
 	for !rl.WindowShouldClose() {
 		rl.UpdateCamera(&camera, rl.CameraFirstPerson)
-
-		nav.HandleBlockInput(&camera, w)
 
 		if rl.IsKeyPressed(rl.KeySpace) && isGrounded {
 			verticalVelocity = jumpForce
@@ -84,11 +58,17 @@ func main() {
 
 		rl.BeginMode3D(camera)
 
-		w.Draw()
+		// rl.DrawCube(rl.NewVector3(0.0, 1.0, 0.0), 2.0, 2.0, 2.0, rl.Blue)
+		// rl.DrawCubeWires(rl.NewVector3(0.0, 1.0, 0.0), 2.0, 2.0, 2.0, rl.DarkBlue)
+		world.RenderChunk(generatedChunk)
 
-		rl.DrawGrid(10, 1.0)
+		rl.DrawGrid(20, 1.0)
 
 		rl.EndMode3D()
+
+		rl.DrawFPS(10, 10)
+		rl.DrawText("WASD - Kretanje | Mis - Okretanje | Space - Skakanje", 10, 40, 20, rl.DarkGray)
+
 		rl.EndDrawing()
 	}
 }
