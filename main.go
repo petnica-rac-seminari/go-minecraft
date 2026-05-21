b0bedc36883d5810142e69d06cb8a8137a468c4b
Mihajlo Djordjevic <midofey.official@gmail.com>
2026-05-21 22:08:03 +0200

    Merge branch 'reljef' into 'grafika'

--- main.go
+++ main.go
@@ -2,12 +2,38 @@ package main
 
 import (
 	reljef "main/Reljef"
+	"main/blocks"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
 
 	//"main/blocks"
 	"main/world"
 )
+
+func RenderBlock(block blocks.Block, x, y, z int) {
+	switch block {
+	case blocks.Air:
+		return
+	case blocks.Grass:
+		rl.DrawCube(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.DarkGreen)
+		rl.DrawCubeWires(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.Green)
+	case blocks.Stone:
+		rl.DrawCube(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.Gray)
+		rl.DrawCubeWires(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.DarkGray)
+	case blocks.Dirt:
+		rl.DrawCube(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.Brown)
+		rl.DrawCubeWires(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.DarkBrown)
+	case blocks.Water:
+		rl.DrawCube(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.Blue)
+		rl.DrawCubeWires(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.DarkBlue)
+	case blocks.Bedrock:
+		rl.DrawCube(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.Black)
+		rl.DrawCubeWires(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.Gray)
+	case blocks.Snow:
+		rl.DrawCube(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.White)
+		rl.DrawCubeWires(rl.NewVector3(float32(x), float32(y), float32(z)), 2.0, 2.0, 2.0, rl.Gray)
+	}
+}
 
 func main() {
 	rl.InitWindow(800, 600, "Raylib Go - 3D Kocka i Skakanje")
