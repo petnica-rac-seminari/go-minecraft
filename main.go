8ff685d568c238803d9ced81f5a624d1621d936b
FikusPetrovic <filip.petrovic.2248@gmail.com>
2026-05-21 22:10:09 +0200

    Clean up

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	defer rl.CloseWindow()
 
 	camera := rl.Camera3D{}
-	camera.Position = rl.NewVector3(4.0, 2.0, 4.0)
+	camera.Position = rl.NewVector3(4.0, 10.0, 4.0)
 	camera.Target = rl.NewVector3(0.0, 1.0, 0.0)
 	camera.Up = rl.NewVector3(0.0, 1.0, 0.0)
 	camera.Fovy = 60.0
@@ -26,7 +26,7 @@ func main() {
 	var verticalVelocity float32 = 0.0
 	const gravity float32 = -0.6
 	const jumpForce float32 = 0.15
-	const groundLevel float32 = 2.0
+	const groundLevel float32 = 10.0
 	var isGrounded bool = true
 
 	generatedChunk := reljef.GenerateChunk(0, 0, 0.1, 8, 0, 1)
