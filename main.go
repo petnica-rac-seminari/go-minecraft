ad7ae3b38f4dc0600b4d4d22ea1ab07d56c8812d
mismih <mismih.g01@gmail.com>
2026-05-21 19:16:45 +0200

    main commits

--- main.go
+++ main.go
@@ -32,21 +32,17 @@ func RenderBlock(block blocks.Block, x, y, z int) {
 }
 
 func main() {
-	// 1. Inicijalizacija prozora
-	rl.InitWindow(800, 600, "Petnica Minecraft")
+	rl.InitWindow(800, 600, "Raylib Go - 3D Kocka i Skakanje")
 	defer rl.CloseWindow()
 
-	// 2. Podešavanje 3D kamere
 	camera := rl.Camera3D{}
-	camera.Position = rl.NewVector3(4.0, 2.0, 4.0) // Početna pozicija kamere
-	camera.Target = rl.NewVector3(0.0, 1.0, 0.0)   // Tačka u koju kamera gleda
-	camera.Up = rl.NewVector3(0.0, 1.0, 0.0)       // Vektor koji definiše gde je "gore"
-	camera.Fovy = 60.0                             // Vidno polje (Field of View)
-	camera.Projection = rl.CameraPerspective       // Vrsta projekcije
+	camera.Position = rl.NewVector3(4.0, 2.0, 4.0)
+	camera.Target = rl.NewVector3(0.0, 1.0, 0.0)
+	camera.Up = rl.NewVector3(0.0, 1.0, 0.0)
+	camera.Fovy = 60.0
+	camera.Projection = rl.CameraPerspective
 
-	// Sakrivamo i zaključavamo kursor kako bismo mogli normalno da se okrećemo mišem (kao u FPS igrama)
 	rl.DisableCursor()
-
 	rl.SetTargetFPS(60)
 
 	var verticalVelocity float32 = 0.0
@@ -55,9 +51,7 @@ func main() {
 	const groundLevel float32 = 2.0
 	var isGrounded bool = true
 
-	// 3. Glavna petlja
 	for !rl.WindowShouldClose() {
-		// Automatski ažurira kameru na osnovu WASD tastera i pokreta miša
 		rl.UpdateCamera(&camera, rl.CameraFirstPerson)
 
 		if rl.IsKeyPressed(rl.KeySpace) && isGrounded {
@@ -79,22 +73,20 @@ func main() {
 			}
 		}
 
-		// --- POČETAK CRTANJA ---
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite)
 
-		// Aktivacija 3D moda
 		rl.BeginMode3D(camera)
 
-		// Crtamo jednu plavu kocku na poziciji (0, 1, 0)
 		rl.DrawCube(rl.NewVector3(0.0, 1.0, 0.0), 2.0, 2.0, 2.0, rl.Blue)
-		// Crtamo ivice kocke kako bi se lakše video 3D oblik
 		rl.DrawCubeWires(rl.NewVector3(0.0, 1.0, 0.0), 2.0, 2.0, 2.0, rl.DarkBlue)
-
-		// Crtamo mrežu na tlu kako bismo imali osećaj za prostor i kretanje
 		rl.DrawGrid(10, 1.0)
 
 		rl.EndMode3D()
+
+		rl.DrawFPS(10, 10)
+		rl.DrawText("WASD - Kretanje | Mis - Okretanje | Space - Skakanje", 10, 40, 20, rl.DarkGray)
+
 		rl.EndDrawing()
 	}
 }
