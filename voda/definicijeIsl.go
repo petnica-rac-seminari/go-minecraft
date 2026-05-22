5545606516c59eb8f364d99a271fa6c7ac1aff23
krompir_official <krompir789654@gmail.com>
2026-05-22 21:04:34 +0200

    dodavanje kordinata i povecavanje osnovnih variabla u struture

--- voda/definicijeIsl.go
+++ voda/definicijeIsl.go
@@ -1,12 +1,25 @@
 package voda
 
+type Cord struct {
+	x int
+	y int
+	z int
+}
+
 type MalaReka struct {
 	rank int
+	deo  []Deo
 }
 
 type VelikaReka struct {
 	rank int
+	deo  []Deo
 }
 
 type Potok struct {
+	deo []Deo
+}
+
+type Deo struct {
+	start [][][]Cord
 }
