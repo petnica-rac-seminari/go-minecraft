02c8e09d5af27ce801d7f0ec77a4deae2ba7b774
vucko0o_ <pedja.kg.1602@gmail.com>
2026-05-22 21:21:04 +0200

    Izbacen nepotreban code iz "cycle.go"

--- dayNightCycle/cycle.go
+++ dayNightCycle/cycle.go
@@ -1,7 +1,6 @@
 package daynightcycle
 
 import (
-	"fmt"
 	"image/color"
 	"math"
 
@@ -20,21 +19,25 @@ func SkyColor(currentTick float32) color.RGBA {
 		A: 255,
 	}
 
-	fmt.Println(c)
 	return c
 }
 
 func RenderSun() (model *rl.Model) {
+
 	sunce_texture := rl.LoadTexture("sunce.png")
 	sunce_mesh := rl.GenMeshSphere(2.0, 32, 32)
+
 	sunce_model := rl.LoadModelFromMesh(sunce_mesh)
 	rl.SetMaterialTexture(sunce_model.Materials, rl.MapDiffuse, sunce_texture)
+
 	return &sunce_model
 }
 
 func RenderMoon() (model *rl.Model) {
+
 	moonTexture := rl.LoadTexture("moon.png")
 	moonMesh := rl.GenMeshSphere(1.5, 32, 32)
+
 	moonModel := rl.LoadModelFromMesh(moonMesh)
 	rl.SetMaterialTexture(moonModel.Materials, rl.MapDiffuse, moonTexture)
 
@@ -44,19 +47,15 @@ func RenderMoon() (model *rl.Model) {
 func SkyBodyAngle(currentTick float32) float32 {
 	currentTime := (currentTick - float32(int(currentTick)/240*240))
 	temp := ((currentTime / 240.0) * 360.0) * math.Pi / 180.0
-	fmt.Println(temp)
 	return temp
 }
 
 func MoveSun(skyBodyAngle float64, camera rl.Camera) rl.Vector3 {
-	temp := rl.Vector3{
+	return rl.Vector3{
 		X: (float32(math.Cos(skyBodyAngle-1))*50 + camera.Position.X),
 		Y: float32(math.Sin(skyBodyAngle-1))*50 + 30,
 		Z: camera.Position.Z,
 	}
-
-	fmt.Println(temp)
-	return temp
 }
 
 func MoveMoon(skyBodyAngle float64, camera rl.Camera) rl.Vector3 {
