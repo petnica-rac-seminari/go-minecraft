613f2a6035b267721d601eafb25f2bcc44e89afb
FikusPetrovic <filip.petrovic.2248@gmail.com>
2026-05-22 22:21:56 +0200

    Flowing water!!! ft. zoe

--- navigation/blockManipulation.go
+++ navigation/blockManipulation.go
@@ -2,8 +2,11 @@ package navigation
 
 import (
 	//"main/blocks"
+	"fmt"
+	"main/blocks"
 	"main/world"
 	"math"
+	"time"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
@@ -26,8 +29,8 @@ func HandleBlockManipulation(camera *rl.Camera3D) {
 			blok := world.GetGlobalBlock(NJK(pos.X), NJK(pos.Y), NJK(pos.Z))
 			if blok >= 2 {
 				if rl.IsMouseButtonDown(rl.MouseButtonLeft) {
-					world.SetGlobalBlock(NJK(pos.X), NJK(pos.Y), NJK(pos.Z), 1)
-
+					world.SetGlobalBlock(NJK(pos.X), NJK(pos.Y), NJK(pos.Z), blocks.Air)
+					updateNeighboringWater(NJK(pos.X), NJK(pos.Y), NJK(pos.Z))
 				} else {
 					/*newBlockBox := rl.BoundingBox{
 						Min: blok.PlaceBlock,
@@ -50,4 +53,29 @@ func HandleBlockManipulation(camera *rl.Camera3D) {
 
 func NJK(f float32) int { //NJK znaci NEKA JEBENA KONVERZIJA IDK
 	return int(math.Round(float64(f)))
+}
+
+func spreadFromNeighbors(x, y, z int) {
+	blocksToCheck := [][3]int{{x, y - 1, z}, {x - 1, y, z}, {x + 1, y, z}, {x, y, z - 1}, {x, y, z + 1}}
+	time.Sleep(time.Millisecond * 300)
+	for _, d := range blocksToCheck {
+		nx, ny, nz := d[0], d[1], d[2]
+		if d[1] == y-1 && world.GetGlobalBlock(d[0], d[1], d[2]) == blocks.Air {
+			world.SetGlobalBlock(d[0], d[1], d[2], blocks.Water)
+		} else if world.GetGlobalBlock(nx, ny, nz) == blocks.Air && world.GetGlobalBlock(x, y, z) == blocks.Water {
+			world.SetGlobalBlock(nx, ny, nz, blocks.Water)
+			go spreadFromNeighbors(nx, ny, nz)
+		}
+	}
+}
+
+func updateNeighboringWater(x, y, z int) {
+	blocksToCheck := [][3]int{{x, y - 1, z}, {x - 1, y, z}, {x + 1, y, z}, {x, y, z - 1}, {x, y, z + 1}, {x, y + 1, z}}
+	for _, d := range blocksToCheck {
+		nx, ny, nz := d[0], d[1], d[2]
+		fmt.Printf("%d %d %d", nx, ny, nz)
+		if world.GetGlobalBlock(nx, ny, nz) == blocks.Water {
+			go spreadFromNeighbors(nx, ny, nz)
+		}
+	}
 }
