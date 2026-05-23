67d1ae90208517ca35d939c29a7bde63b18b2c19
MMI08 <marijailicpro@gmail.com>
2026-05-23 03:13:02 +0200

    pokazivac

--- voda/velikaReka.go
+++ voda/velikaReka.go
@@ -1,12 +1,12 @@
 package voda
 
-func VelikaErekcina(rank int, start Cord, postojece []VelikaReka) *VelikaReka {
+func VelikaErekcina(rank int, start Cord, postojece *[]VelikaReka) *VelikaReka {
 	reka := &VelikaReka{rank: rank}
 	trenutna := start
 	for i := 0; i < 200; i++ {
 		sledeca := Cord{x: trenutna.x, y: trenutna.y + 1, z: trenutna.z}
 		deo := Deo{start: trenutna, end: sledeca}
-		for _, druga := range postojece {
+		for _, druga := range *postojece {
 			if druga.rank < reka.rank {
 				if PipkaReku(sledeca, druga) {
 					reka.Add(&deo)
