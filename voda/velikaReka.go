b61cbdb3e6771015342b7f483174f982ac5720f6
MMI08 <marijailicpro@gmail.com>
2026-05-23 03:27:07 +0200

    rand

--- voda/velikaReka.go
+++ voda/velikaReka.go
@@ -1,10 +1,13 @@
 package voda
 
+import "math/rand"
+
 func VelikaErekcina(rank int, start Cord, postojece *[]VelikaReka) *VelikaReka {
 	reka := &VelikaReka{rank: rank}
 	trenutna := start
 	for i := 0; i < 200; i++ {
-		sledeca := Cord{x: trenutna.x, y: trenutna.y + 1, z: trenutna.z}
+		pomakX := rand.Intn(3) - 1
+		sledeca := Cord{x: trenutna.x + pomakX, y: trenutna.y + 1, z: trenutna.z}
 		deo := Deo{start: trenutna, end: sledeca}
 		for _, druga := range *postojece {
 			if druga.rank < reka.rank {
